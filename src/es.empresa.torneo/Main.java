08db4092c5c6d09842753326ec7f97d71ca60045
luisjrz15 <luis14458@hotmail.com>
2025-04-27 21:44:12 +0200

    Add files via upload
    Main.java

---
+++ src/es.empresa.torneo/Main.java
@@ -0,0 +1,10 @@
+package es.empresa.torneo;
+
+import es.empresa.torneo.vista.VistaPrincipal;
+
+public class Main {
+    public static void main(String[] args) {
+        VistaPrincipal vista = new VistaPrincipal();
+        vista.mostrarMenu();
+    }
+}
